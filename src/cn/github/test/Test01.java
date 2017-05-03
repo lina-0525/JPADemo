aa2d3d7d34800664e400342e577e231f63a3dc7c
lina <1016596165@qq.com>
2017-05-03 11:52:36 +0800

    create file 01

---
+++ src/cn/github/test/Test01.java
@@ -0,0 +1,11 @@
+package cn.github.test;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello github 01");
+
+	}
+
+}
