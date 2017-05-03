4bb62ab8f5ba611677cc01d4697641d94456b213
lina <1016596165@qq.com>
2017-05-03 14:17:17 +0800

    update file 02

--- src/cn/github/test/Test01.java
+++ src/cn/github/test/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello github 01");
+		System.out.println("localServer update here 0719");
 
 	}
 
